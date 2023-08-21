690d6105f2aea5fcbdde03e48f25e31eae4899ea
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2023-08-21 20:16:31 -0300

    Feat - integration with chat-gpt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,9 @@
-"use client";
-
-import { Avatar } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Chat } from "@/components/Chat";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-50">
-      <Card className="w-[440px] h-[700px] grid grid-rows-[min-content_1fr_min-content]">
-        <CardHeader>
-          <CardTitle>Chat AI</CardTitle>
-          <CardDescription>
-            Using Versl SDK to create a chat bot.
-          </CardDescription>
-        </CardHeader>
-
-        <CardContent className="space-y-4">
-          <div className="flex gap-3">
-            <Avatar>
-              <AvatarFallback>EA</AvatarFallback>
-              {/* <AvatarImage src="https://avatars.githubusercontent.com/u/17826831?s=400&u=8a3dae2f79fe6e61d08963831b4b75a7ed763e04&v=4" /> */}
-              <AvatarImage src="https://github.com/eduardo123andrade.png" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold to-slate-700">Human:</span>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              nostrum, dolorum sapiente dolore velit commodi, quo, numquam
-              dolores eius doloribus maiores consectetur sunt illum. Eveniet
-              architecto dolorum nulla cupiditate laboriosam.
-            </p>
-          </div>
-
-          <div className="flex gap-3">
-            <Avatar>
-              <AvatarFallback>WB</AvatarFallback>
-              <AvatarImage src="https://i.natgeofe.com/k/093c14b4-978e-41f7-b1aa-3aff5d1c608a/gray-wolf-closeup_3x4.jpg" />
-            </Avatar>
-            <p className="leading-relaxed">
-              <span className="block font-bold to-slate-700">Wolf bot:</span>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
-              nostrum, dolorum sapiente dolore velit commodi, quo, numquam
-              dolores eius doloribus maiores consectetur sunt illum. Eveniet
-              architecto dolorum nulla cupiditate laboriosam.
-            </p>
-          </div>
-        </CardContent>
-
-        <CardFooter className="space-x-2">
-          <Input placeholder="How can I help you" />
-
-          <Button type="submit">Send</Button>
-        </CardFooter>
-      </Card>
+      <Chat />
     </div>
   );
 }
---
+++ src/components/Chat.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { useChat } from "ai/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { ScrollArea } from "./ui/scroll-area";
+
+export const Chat = () => {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat", //Default
+  });
+
+  return (
+    <Card className="w-[440px]">
+      <CardHeader>
+        <CardTitle>Chat AI</CardTitle>
+        <CardDescription>Using Versl SDK to create a chat bot.</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <ScrollArea className="h-[640px] w-full space-y-4">
+          {messages.map((message) => {
+            return (
+              <div key={message.id} className="flex gap-3">
+                {message.role === "user" && (
+                  <Avatar>
+                    <AvatarFallback>EA</AvatarFallback>
+                    <AvatarImage src="https://github.com/eduardo123andrade.png" />
+                  </Avatar>
+                )}
+
+                {message.role === "system" && (
+                  <Avatar>
+                    <AvatarFallback>WB</AvatarFallback>
+                    <AvatarImage src="https://i.natgeofe.com/k/093c14b4-978e-41f7-b1aa-3aff5d1c608a/gray-wolf-closeup_3x4.jpg" />
+                  </Avatar>
+                )}
+                <p className="leading-relaxed">
+                  <span className="block font-bold to-slate-700">
+                    {message.role === "user" ? "User" : "Wolf bot"}
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            );
+          })}
+        </ScrollArea>
+      </CardContent>
+
+      <CardFooter>
+        <form onSubmit={handleSubmit} className="w-full flex gap-2">
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            placeholder="How can I help you"
+          />
+
+          <Button className="bg-black text-white" type="submit">
+            Send
+          </Button>
+        </form>
+      </CardFooter>
+    </Card>
+  );
+};
